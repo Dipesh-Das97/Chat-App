615568dcdd6a4260492ec239d7c3f1e911cafee2
Dipesh <dipesh@Dipeshs-MacBook-Pro.local>
2023-05-18 19:02:53 +0530

    push package json changes

--- src/@types/user/index.d.ts
+++ src/@types/user/index.d.ts
@@ -1,6 +1,8 @@
-import { UserInfoShape } from "../../interfaces/userInfo";
+// import { UserInfoShape } from "../../interfaces/userInfo";
 
+// @ts-ignore
 declare module 'Express' {
+    // @ts-ignore
     export interface Request {
         // @ts-ignore
         userInfo?: any
