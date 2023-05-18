e717b005e3279a578e0c26b1f81f08fe8924212d
Dipesh <dipesh@Dipeshs-MacBook-Pro.local>
2023-05-18 18:32:36 +0530

    push action changes

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ app.get('/', (req: Request, res: Response)=>{
 
 app.post('/user', addUserInfoToReq , (req: Request, res: Response) => {
     const lastName = req?.body.lastName;
+    //@ts-ignore
     let userInfo = req?.userInfo;
     res.send({...userInfo, name: userInfo?.firstName + ' ' + lastName});
 });
